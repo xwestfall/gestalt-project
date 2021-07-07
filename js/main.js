c2b571818bfa37b0762b24bfdc7a39bf6bb3f8e1
danilo <58399312+xwestfall@users.noreply.github.com>
2021-07-07 17:37:01 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const dropdownLink = document.querySelectorAll('.nav-link');
+  dropdownLink.forEach(menu => {
+    ['touchstart', 'click'].forEach(userEvent => {
+      menu.addEventListener(userEvent, handleClick);
+    });
+  });
+
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.add('active');
+  };
